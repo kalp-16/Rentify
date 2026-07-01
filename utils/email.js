433134c9973a06e2243c6db9f696d3f76bee25b9
Your Name <you@example.com>
2026-07-01 21:22:59 +0530

    Use Brevo SMTP

--- utils/email.js
+++ utils/email.js
@@ -1,40 +1,3 @@
-/*const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-});
-
-// Verify SMTP connection once when the app starts
-transporter.verify((err, success) => {
-    if (err) {
-        console.error("SMTP Error:", err);
-    } else {
-        console.log("SMTP Connected");
-    }
-});
-
-const sendEmail = async (
-    to,
-    subject,
-    html
-) => {
-    if (!to) {
-        throw new Error("Missing email recipient.");
-    }
-    await transporter.sendMail({
-        from: `"Rentify Notifications" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        html
-    });
-};
-module.exports = sendEmail;*/
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
@@ -43,14 +6,21 @@ const transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: process.env.BREVO_USER,
-        pass: process.env.BREVO_PASS
-    }
+        pass: process.env.BREVO_PASS,
+    },
 });
 
-await transporter.sendMail({
-    from: `"Rentify" <${process.env.BREVO_SENDER}>`,
-    to,
-    subject,
-    html
-});
+const sendEmail = async (to, subject, html) => {
+    if (!to) {
+        throw new Error("Missing email recipient.");
+    }
+
+    await transporter.sendMail({
+        from: `"Rentify" <${process.env.BREVO_SENDER}>`,
+        to,
+        subject,
+        html,
+    });
+};
+
 module.exports = sendEmail;
