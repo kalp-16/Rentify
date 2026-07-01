72ece439f3e8991d8bf1cfa5a6d43440c8e5e0ca
Your Name <you@example.com>
2026-07-01 21:17:12 +0530

    Use Brevo SMTP

--- utils/email.js
+++ utils/email.js
@@ -38,39 +38,19 @@ module.exports = sendEmail;*/
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-
     host: "smtp-relay.brevo.com",
-
     port: 587,
-
     secure: false,
-
     auth: {
-
         user: process.env.BREVO_USER,
-
         pass: process.env.BREVO_PASS
-
     }
-
 });
 
-const sendEmail = async (to, subject, html) => {
-
-    if (!to) {
-        throw new Error("Missing email recipient.");
-    }
-
-    await transporter.sendMail({
-
-        from: `"Rentify" <${process.env.BREVO_USER}>`,
-
-        to,
-        subject,
-        html
-
-    });
-
-};
-
+await transporter.sendMail({
+    from: `"Rentify" <${process.env.BREVO_SENDER}>`,
+    to,
+    subject,
+    html
+});
 module.exports = sendEmail;
