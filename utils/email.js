8891335fdf4276db8ccabcc0591c3210a57816de
Your Name <you@example.com>
2026-07-01 19:37:07 +0530

    Use Brevo SMTP

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+/*const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -34,4 +34,43 @@ const sendEmail = async (
         html
     });
 };
+module.exports = sendEmail;*/
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+
+    host: "smtp-relay.brevo.com",
+
+    port: 587,
+
+    secure: false,
+
+    auth: {
+
+        user: process.env.BREVO_USER,
+
+        pass: process.env.BREVO_PASS
+
+    }
+
+});
+
+const sendEmail = async (to, subject, html) => {
+
+    if (!to) {
+        throw new Error("Missing email recipient.");
+    }
+
+    await transporter.sendMail({
+
+        from: `"Rentify" <${process.env.BREVO_USER}>`,
+
+        to,
+        subject,
+        html
+
+    });
+
+};
+
 module.exports = sendEmail;
